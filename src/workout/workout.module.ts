2d4dbe3df1a73f4d5371ca78a9db8f1310320c6c
jajoh143 <jajoh143@gmail.com>
2024-10-03 15:31:00 -0500

    fixed issue with workout controller dependency missing

--- src/workout/workout.module.ts
+++ src/workout/workout.module.ts
@@ -5,9 +5,11 @@ import { WorkoutController } from './workout.controller';
 import { WorkoutService } from './workout.service';
 import { Workout } from 'src/data/entities/workout.entity';
 import { WorkoutExercise } from 'src/data/entities/workout-exercise.entity';
+import { UserWorkout } from 'src/data/entities/user-workout.entity';
+import { Exercise } from 'src/data/entities/exercise.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Workout, User, WorkoutExercise])],
+  imports: [TypeOrmModule.forFeature([Exercise, Workout, WorkoutExercise, User, UserWorkout])],
   controllers: [WorkoutController],
   providers: [WorkoutService],
 })
